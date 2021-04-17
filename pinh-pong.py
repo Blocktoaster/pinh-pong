7c5d3d888474e07a3c0b50671f3a5a31a7ca7578
BlockToaster <82261925+Blocktoaster@users.noreply.github.com>
2021-04-17 14:55:30 +0500

    Update pinh-pong.py

--- pinh-pong.py
+++ pinh-pong.py
@@ -1,5 +1,17 @@
 from pygame import*
+from random import*
+
 window = display.set_mode((700,500))
+
 display.set_caption("Ping Ping")
-#нет
 ball = background = transform.scale(image.load("Trup.jpg"),(100,100))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65,66))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x,self.rect.y))
